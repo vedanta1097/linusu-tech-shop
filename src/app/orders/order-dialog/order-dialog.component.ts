0c473a5d57abbd36374cc269f00d725f11532ec4
vedanta1097 <vedanta.maha@gmail.com>
2019-11-10 11:38:21 +0800

    data seed

--- src/app/orders/order-dialog/order-dialog.component.ts
+++ src/app/orders/order-dialog/order-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
-import { Order } from '../orders.model';
+import { Order, Item } from '../orders.model';
 
 @Component({
   selector: 'app-order-dialog',
@@ -10,8 +10,35 @@ import { Order } from '../orders.model';
 })
 export class OrderDialogComponent implements OnInit {
 
+  categories = ['CPU', 'Motherboard', 'Video Card', 'Memory'];
+
+  cpuList: Item[] = [
+    { category: 'CPU', name: 'AMD Ryzen 5 2600', price: 117 },
+    { category: 'CPU', name: 'Intel Core i5-9600K', price: 229 },
+    { category: 'CPU', name: 'AMD RYZEN 5 3600', price: 194 },
+  ];
+
+  motherBoardList: Item[] = [
+    { category: 'Motherboard', name: 'MSI PRO Z390-A', price: 129 },
+    { category: 'Motherboard', name: 'ASUS PRIME B360M-A', price: 84 },
+    { category: 'Motherboard', name: 'ASRock B450M PRO4 AM4', price: 79 },
+  ];
+
+  videoCardList: Item[] = [
+    { category: 'Video Card', name: 'ZOTAC GeForce GTX 1060', price: 209 },
+    { category: 'Video Card', name: 'MSI Radeon RX 580', price: 189 },
+    { category: 'Video Card', name: 'GIGABYTE GeForce RTX 2070', price: 499 },
+  ];
+
+  memoryList: Item[] = [
+    { category: 'Memory', name: 'CORSAIR Vengeance RGB Pro 16GB', price: 97 },
+    { category: 'Memory', name: 'G.SKILL TridentZ RGB Series 16GB', price: 86 },
+    { category: 'Memory', name: 'G.SKILL Ripjaws Series 8GB', price: 42 },
+  ];
+
   orderData: Order;
   orderForm: FormGroup;
+  itemList = [];
 
   constructor(
     public dialogRef: MatDialogRef<OrderDialogComponent>,
